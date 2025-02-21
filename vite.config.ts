140d4b6b053d687f43acccf8869a9c431f9f0806
peijiezh <choupeijie@gmail.com>
2025-02-21 15:26:07 +0800

    some modify for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()]
-})
+const config = {
+  server: {
+    port: 3000
+  },
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
+}
+
+export default defineConfig(config)
