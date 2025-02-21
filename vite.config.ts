acf70b5b6a55558d77de4667b56f9cb7eaff4cb3
peijiezh <choupeijie@gmail.com>
2025-02-21 14:55:40 +0800

    vite config file have add output

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  server: {
+    port: 3000
+  },
   plugins: [react()],
   base: '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
 })
